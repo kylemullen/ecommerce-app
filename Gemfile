93aff583fb803e91490fd373f6a2c46d19327625
“kylemullengit config --global user.email “kylem.mullen.com <kylem.mullen@gmail.com>
2014-10-26 14:06:44 -0500

    Heroku Setup

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source 'https://rubygems.org'
 
+gem 'pg', group: :production
+gem 'rails_12factor', group: :production
 gem 'seed_dump'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.5'
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,8 @@
 class OrdersController < ApplicationController
 
 	def create
-		prodct = Product.find(params[:order][:product_id].to_i)
+		
+		product = Product.find(params[:order][:product_id])
 		total_price = product.price * params[:order][:quantity].to_i
 		@order = current_user.orders.create(params[:order].merge({:total => total_price}))
 
